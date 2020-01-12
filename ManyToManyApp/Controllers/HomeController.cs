453267802406f354e48ce387e10c09baa9e401e9
мария пугачёва <mawytochka99@mail.ru>
2020-01-12 20:19:56 +0300

    Добавьте файлы проекта.

---
+++ ManyToManyApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManyToManyApp.Models;
+
+namespace ManyToManyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private StudentContext db = new StudentContext();
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        public ActionResult Details(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
