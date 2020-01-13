3def1fb7ca34719efdb94c1030c202520884701a
мария пугачёва <mawytochka99@mail.ru>
2020-01-13 21:01:31 +0300

    add "Edit" functionality

--- ManyToManyApp/Controllers/HomeController.cs
+++ ManyToManyApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ManyToManyApp.Models;
+using System.Data.Entity;
 
 namespace ManyToManyApp.Controllers
 {
@@ -13,6 +14,39 @@ namespace ManyToManyApp.Controllers
         public ActionResult Index()
         {
             return View(db.Students.ToList());
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Courses = db.Courses.ToList();
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student, int[] selectedCourses)
+        {
+            Student newStudent = db.Students.Find(student.Id);
+            newStudent.Name = student.Name;
+            newStudent.Surname = student.Surname;
+
+            newStudent.Courses.Clear();
+            if (selectedCourses != null)
+            {
+                foreach (var c in db.Courses.Where(course => selectedCourses.Contains(course.Id)))
+                {
+                    newStudent.Courses.Add(c);
+                }
+            }
+
+            db.Entry(newStudent).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult Details(int id = 0)
